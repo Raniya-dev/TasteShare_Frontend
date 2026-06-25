b00f4d0e1e0ff48fc2831e9a465244ab34c927d9
raniyadev <raniyanaser77@gmail.com>
2026-06-25 18:30:15 +0530

    changed the basename

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Recipe_finder_platform/",
+  base:"/TastShare_Frontend/",
 })
